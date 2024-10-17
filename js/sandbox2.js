4a2317dd6a23b838c59be87716c78cc0063caec3
Jon Ayala <126JonathanAyala@frhsd.com>
2024-10-17 13:54:45 -0400

    switch statements

--- js/sandbox2.js
+++ js/sandbox2.js
@@ -1,31 +1,4 @@
-// if statements
 
-// const age = 25
-
-// if(age > 20){
-//     console.log('you are over 20 years old')
-// }
-
-// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-
-// if(ninjas.length > 4) {
-//     console.log("that's a lot of ninjas");
-// }
-
-// const password = 'p@ssword';
-
-// if(password.length >= 12 && password.includes('@')){
-//      console.log('that password is mighty strong');
-// } else if(password.length >= 8 || password.includes('@') && password.length >= 5){
-//      console.log('that password is long enough!')
-//  } else {
-//      console.log('password is not long enough')
-//  }
-
-// const d = new Date();
-// let hour = d.getHours();
-
-// console.log(hour);
 
 // if (hour <12) {
 //     console.log('It is morning.')
@@ -35,17 +8,23 @@
 //     console.log('it is night time.')
 // }
 
-const scores = [50, 25, 0, 30, 100, 20, 10];
+const grade = '50';
 
-for(let i = 0; I < scores.length; i++) {
 
-    if(scores[i] === 0) {
-        console.log('your score: ', scores[i]);
-        continue;
-    }
-
-    if(scores[i] === 100) {
-        console.log('congrats, you got the top score');
+switch(grade){
+    case 50:
+        console.log('you got an A!');
         break;
-    }
+    case 'B':
+        console.log('you got an B!');
+        break;
+    case "C":
+        console.log('you got an C!');
+        break;
+    case "D":
+        console.log('you got an D!');
+        break;
+    case 'E':
+        console.log('you got an E!');
+
 }
