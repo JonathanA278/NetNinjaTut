da877a3361e7ea82512c9902afc75880f32dd6db
Jon Ayala <126JonathanAyala@frhsd.com>
2024-10-17 13:48:07 -0400

    Logicals

--- js/sandbox2.js
+++ js/sandbox2.js
@@ -12,25 +12,25 @@
 //     console.log("that's a lot of ninjas");
 // }
 
-// const password = 'p@ssword';
+const password = 'p@ssword';
 
-// if(password.length >= 12){
-//     console.log('that password is mighty strong');
-// } else if(password.length >= 8){
-//     console.log('that password is long enough!')
+if(password.length >= 12 && password.includes('@')){
+     console.log('that password is mighty strong');
+} else if(password.length >= 8 || password.includes('@') && password.length >= 5){
+     console.log('that password is long enough!')
+ } else {
+     console.log('password is not long enough')
+ }
+
+// const d = new Date();
+// let hour = d.getHours();
+
+// console.log(hour);
+
+// if (hour <12) {
+//     console.log('It is morning.')
+// } else if  (hour < 17) {
+//     console.log('It is afternoon.')
 // } else {
-//     console.log('password is not long enough')
+//     console.log('it is night time.')
 // }
-
-const d = new Date();
-let hour = d.getHours();
-
-console.log(hour);
-
-if (hour <12) {
-    console.log('It is morning.')
-} else if  (hour < 17) {
-    console.log('It is afternoon.')
-} else {
-    console.log('it is night time.')
-}
